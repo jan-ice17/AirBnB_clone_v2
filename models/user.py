0097d32900b868a2dcdcd32f940e4cdba62805c1
jan_ice17 <judysjewel17@gmail.com>
2024-06-26 23:07:32 +0300

    models/user.py updated

--- models/user.py
+++ models/user.py
@@ -15,11 +15,21 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
         """
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    if models.storage_type == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    places = relationship("Place", backref="user")
-    reviews = relationship("Review", backref="user")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
