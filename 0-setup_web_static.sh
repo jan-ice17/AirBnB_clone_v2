abc2136b447027e19a03f3e92e6a7c7bf084957d
edison pryce <edisonpryce@gmail.com>
2024-07-14 13:21:36 -0700

    Kep it clean

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# web_static deployment setting up
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
