855756cc1d6e327367e2e7e190b427e8c721265d
edison pryce <edisonpryce@gmail.com>
2024-07-15 12:14:59 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,3 +17,4 @@ chown -R ubuntu:ubuntu /data
 sed -i "/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 sudo ufw allow "Nginx HTTP"
 sudo service nginx restart
+exit 1
