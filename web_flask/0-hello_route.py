714a2909fb92dcae1f160b303c0cf62ae19fb01d
jan-ice17 <judysjewel17@gmail.com>
2024-07-31 19:55:29 +0300

    pycode fixed in file0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,13 +4,16 @@
 A script that starts a Flask web application:
 and runs on port 5000
 """
+
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def home():
     return('Hello HBNB!')
 
+
 if __name__ == '__main__':
-    app.run (host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
